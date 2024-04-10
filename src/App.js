aaae4e18941e0f9e6c9600b81c5c1fc874c8046d
Rubyseher <67371345+Rubyseher@users.noreply.github.com>
2024-04-10 23:14:26 +0530

    leaf works

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import Dropdown from 'react-bootstrap/Dropdown';
 import Container from 'react-bootstrap/Container';
 import Navbar from 'react-bootstrap/Navbar';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faLeaf } from '@fortawesome/free-solid-svg-icons'
 import './App.css';
 
 function App() {
@@ -11,21 +12,30 @@ function App() {
       <Navbar bg="success" data-bs-theme="dark" >
         <Container style={{ margin: '5px 0px 5px 50px ' }}>
           <Navbar.Brand href="#home">
-          <FontAwesomeIcon icon="fa-solid fa-leaf" />
+          <FontAwesomeIcon icon={faLeaf} size='xl' style={{ marginRight: '15px' }} color="var(--blue)" />
             <b>Team 13</b>
           </Navbar.Brand>
         </Container>
       </Navbar>
-
+      <br/>
       <Dropdown>
         <Dropdown.Toggle variant="success" id="dropdown-basic">
-          Dropdown Button
+          Select Month
         </Dropdown.Toggle>
 
         <Dropdown.Menu>
-          <Dropdown.Item href="#/action-1">Action</Dropdown.Item>
-          <Dropdown.Item href="#/action-2">Another action</Dropdown.Item>
-          <Dropdown.Item href="#/action-3">Something else</Dropdown.Item>
+          <Dropdown.Item href="#/action-1">January</Dropdown.Item>
+          <Dropdown.Item href="#/action-2">Feb</Dropdown.Item>
+          <Dropdown.Item href="#/action-3">March</Dropdown.Item>
+          <Dropdown.Item href="#/action-1">April</Dropdown.Item>
+          <Dropdown.Item href="#/action-2">May</Dropdown.Item>
+          <Dropdown.Item href="#/action-3">June</Dropdown.Item>
+          <Dropdown.Item href="#/action-1">July</Dropdown.Item>
+          <Dropdown.Item href="#/action-2">Aug</Dropdown.Item>
+          <Dropdown.Item href="#/action-3">Sep</Dropdown.Item>
+          <Dropdown.Item href="#/action-1">Oct</Dropdown.Item>
+          <Dropdown.Item href="#/action-2">Nov</Dropdown.Item>
+          <Dropdown.Item href="#/action-3">Dec</Dropdown.Item>
         </Dropdown.Menu>
       </Dropdown>
     </div>

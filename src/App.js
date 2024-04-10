1b125104215809255fe32bf6b9dd4b6d80ef422b
Rubyseher <67371345+Rubyseher@users.noreply.github.com>
2024-04-10 23:07:40 +0530

    leaf

--- src/App.js
+++ src/App.js
@@ -2,20 +2,32 @@ import 'bootstrap/dist/css/bootstrap.min.css';
 import Dropdown from 'react-bootstrap/Dropdown';
 import Container from 'react-bootstrap/Container';
 import Navbar from 'react-bootstrap/Navbar';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <Navbar bg="success" data-bs-theme="dark">
-        <Container>
-          <Navbar.Brand href="#home"><b>Team 13</b></Navbar.Brand>
+    <div >
+      <Navbar bg="success" data-bs-theme="dark" >
+        <Container style={{ margin: '5px 0px 5px 50px ' }}>
+          <Navbar.Brand href="#home">
+          <FontAwesomeIcon icon="fa-solid fa-leaf" />
+            <b>Team 13</b>
+          </Navbar.Brand>
         </Container>
       </Navbar>
 
-      <Dropdown.Toggle variant="success" id="dropdown-basic">
-        Dropdown Button
-      </Dropdown.Toggle>
+      <Dropdown>
+        <Dropdown.Toggle variant="success" id="dropdown-basic">
+          Dropdown Button
+        </Dropdown.Toggle>
+
+        <Dropdown.Menu>
+          <Dropdown.Item href="#/action-1">Action</Dropdown.Item>
+          <Dropdown.Item href="#/action-2">Another action</Dropdown.Item>
+          <Dropdown.Item href="#/action-3">Something else</Dropdown.Item>
+        </Dropdown.Menu>
+      </Dropdown>
     </div>
   );
 }
